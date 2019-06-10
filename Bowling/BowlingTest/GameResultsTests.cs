ce80f729f41d48b65e0c1bf073b7808adb93bdbc
Darya <makarenko.darya.work@gmail.com>
2019-06-11 00:27:06 +0300

    add score test for const number of pins, no bonuses

--- Bowling/BowlingTest/GameResultsTests.cs
+++ Bowling/BowlingTest/GameResultsTests.cs
@@ -23,6 +23,25 @@ namespace BowlingTest
             game.Roll(pins);
         }
 
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        public void ConstantScoreGameTest(int pins)
+        {
+            RollDefinedNumberOfPins(20, pins);
+            int score = game.Score();
+            Assert.Equal(score, 20 * pins);
+        }
+
+        private void RollDefinedNumberOfPins(int times, int pins)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
         public void Dispose()
         {
             game = null;
