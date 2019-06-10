1f03567a4b0c2051327e48abe637e1d9a316f40a
Darya <makarenko.darya.work@gmail.com>
2019-06-10 19:51:35 +0300

    add class definition with initialization tests

---
+++ Bowling/Bowling/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+        public int Score()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Bowling/BowlingTest/UnitTest1.cs
+++ Bowling/BowlingTest/UnitTest1.cs
@@ -1,14 +1,40 @@
 using System;
+using Bowling;
 using Xunit;
 
 namespace BowlingTest
 {
-    public class UnitTest1
+    public class InitializationTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private Game game;
 
+        public InitializationTests()
+        {
+            game = new Game();
+        }
+
+        [Fact]
+        public void NewGameNotNull()
+        {
+            Assert.NotNull(game);
+        }
+
+        [Fact]
+        public void RollMethodIsAvailable()
+        {
+            var gameType = game.GetType();
+            var methodInfo = gameType.GetMethod("Roll");
+
+            Assert.NotNull(methodInfo);
+        }
+
+        [Fact]
+        public void ScoreMethodIsAvailable()
+        {
+            var gameType = game.GetType();
+            var methodInfo = gameType.GetMethod("Score");
+
+            Assert.NotNull(methodInfo);
         }
     }
 }
