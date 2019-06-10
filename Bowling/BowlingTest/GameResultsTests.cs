9ea2c94069eddbc1470d6924201d2dea843fdb6d
Darya <makarenko.darya.work@gmail.com>
2019-06-11 00:09:28 +0300

    add game results tests section, make one roll test

---
+++ Bowling/BowlingTest/GameResultsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Bowling;
+using Xunit;
+
+namespace BowlingTest
+{
+    public class GameResultsTests: IDisposable
+    {
+        private Game game;
+
+        public GameResultsTests()
+        {
+            game = new Game();
+        }
+
+        [Theory]
+        [InlineData(6)]
+        [InlineData(5)]
+        [InlineData(0)]
+        public void OneRollTest(int pins)
+        {
+            game.Roll(pins);
+        }
+
+        public void Dispose()
+        {
+            game = null;
+        }
+    }
+}
